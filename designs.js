2ef953fb71907b141e1c67cc3d4e82fe7ccb03dd
mbkoshy <42899737+mbkoshy@users.noreply.github.com>
2020-06-13 21:57:20 -0500

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,10 +1,53 @@
-// Select color input
-// Select size input
+document.addEventListener('DOMContentLoaded', function() {
+  // trigger functions when submit button is clicked
+  const submitButton = document.querySelector('input[type="submit"]');
+  submitButton.addEventListener('click', makeGrid);
 
-// When size is submitted by the user, call makeGrid()
+  function makeGrid(event) {
+    // stop the page from refreshing when clicked submit
+    event.preventDefault();
 
-function makeGrid() {
+    // Select color input (save it here so user can't change it later)
+    const colorPicker = document.querySelector('#colorPicker');
+    const color = colorPicker.value;
 
-// Your code goes here!
+    // Select size input
+    const height = parseInt(document.querySelector('#inputHeight').value);
+    const width = parseInt(document.querySelector('#inputWidth').value);
 
-}
+    const canvas = document.querySelector('#pixelCanvas');
+    const grid = makeTable(height, width);
+    canvas.innerHTML = (grid);
+
+    // Add click listeners to change cell colors
+    const cells = document.querySelectorAll('#pixelCanvas td');
+    cells.forEach(function(cell) {
+      cell.addEventListener('click', function(event) {
+        const cell = event.target;
+
+        if (cell.className === '') {
+          cell.className = 'filled';
+          cell.bgColor = color;
+        }
+        else {
+          cell.className = '';
+          cell.bgColor = '#FFFFFF';
+        }
+      })
+    });
+
+    function makeTable(height, width) {
+      let columns = '';
+      for (let i = 0; i < width; i++) {
+        columns += '<td></td>'
+      }
+
+      let rows = '';
+      for (let i = 0; i < height; i++) {
+        rows += '<tr>' + columns + '</tr>'
+      }
+
+      return rows;
+    }
+  }
+});
